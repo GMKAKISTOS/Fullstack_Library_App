256ab209a70c7be9a4bfb1f3725f451f8f5c476b
Panagiotis_ch <panos_chron@hotmail.com>
2026-04-22 14:54:34 +0300

    Add files via upload

---
+++ library-app/02-backend/spring-boot-library/src/main/java/com/backend_library/spring_boot_library/entity/Book.java
@@ -0,0 +1,42 @@
+package com.backend_library.spring_boot_library.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+//import javax.*;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "author")
+    private String author;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "copies")
+    private int copies;
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+    @Column(name = "category")
+    private String category;
+    @Column(name = "img")
+    private String img;
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public void setCopiesAvailable(int copiesAvailable) {
+        this.copiesAvailable = copiesAvailable;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ library-app/02-backend/spring-boot-library/src/main/java/com/backend_library/spring_boot_library/entity/Review.java
@@ -0,0 +1,54 @@
+package com.backend_library.spring_boot_library.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "review")
+@Data
+public class Review {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id")
+private Long id;
+
+@Column(name = "user_email")
+private String userEmail;
+
+@Column(name = "date")
+@CreationTimestamp
+private Date date;
+
+@Column(name = "rating")
+private double rating;
+
+@Column(name = "book_id")
+private Long bookId;
+
+@Column(name = "review_description")
+private String reviewDescription;
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public void setReviewDescription(String reviewDescription){
+        this.reviewDescription = reviewDescription;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
